ae010612a6b2ab5b473673efe3db2e150bc6efd3
Daniel Li <iammrwiggles@Daniels-MacBook-Pro.local>
2017-01-24 18:44:03 -0800

    initial python backend commit

---
+++ apps/powderApp/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^add_user$', views.add_user),
+    url(r'^success$', views.success),
+    url(r'^login$', views.login),
+    url(r'^logout$', views.logout),
+    url(r'^tasks$',views.tasks),
+]
